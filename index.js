48b80e8f486940360d060b3e7646e23fc97fae8b
Giurllan Pimentel <146568002+Giurllanpimentel@users.noreply.github.com>
2024-10-16 20:41:32 -0300

    Adicionado uma logica basica

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let taboada = parseInt(prompt("Digite um numero"))
+
+contador = 1
+
+while (contador <= 10) {
+   document.write(taboada + "X" + contador + "=" + taboada * contador + "<br>")
+   contador++ 
+}
