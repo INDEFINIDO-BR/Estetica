f7a37c8b5c94f2324f26a83786c5a4d5e6dfd618
Giurllan Pimentel <146568002+Giurllanpimentel@users.noreply.github.com>
2024-10-18 11:42:43 -0300

    Feito o header

--- index.js
+++
@@ -1,9 +0,0 @@
-
-let taboada = parseInt(prompt("Digite um numero"))
-
-contador = 1
-
-while (contador <= 10) {
-   document.write(taboada + "X" + contador + "=" + taboada * contador + "<br>")
-   contador++ 
-}
